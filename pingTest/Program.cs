8342ec328254da6d41495eb33c6ca0297c07beba
Theo zibell <theo@zibell.se>
2022-05-03 14:45:34 +0200

    Ping test fungera

---
+++ pingTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+
+Ping p1 = new Ping();
+string data = "Die lol and play lol aaaaaaaaaaa";
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+PingReply PR = p1.Send("10.156.8.80", 120, buffer);
+
+System.Console.WriteLine(PR.Status);
+Console.ReadLine();
